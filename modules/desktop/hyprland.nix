4e5bb2a574f2a1fbbcad02d988e202043b15e634
Sooraj Ep <epsooraj4@gmail.com>
2026-08-08 22:47:54 +0530

    Update waybar

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -1,3 +1,22 @@
+{ pkgs, ... }:
+
 {
-  programs.hyprland.enable = true;
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+  };
+
+  environment.systemPackages = [ pkgs.waybar ];
+
+  systemd.user.services.waybar = {
+    description = "Waybar status bar";
+    wantedBy = [ "graphical-session.target" ];
+    partOf = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.waybar}/bin/waybar";
+      ExecReload = "${pkgs.coreutils}/bin/kill -SIGUSR2 $MAINPID";
+      Restart = "on-failure";
+    };
+  };
 }
